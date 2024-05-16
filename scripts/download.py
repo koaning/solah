cd59871bbf62ae2337d5e1116d624082a55c5afb
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-05-16 21:31:28 +0000

    start

--- scripts/download.py
+++ scripts/download.py
@@ -2,11 +2,10 @@
 # pip install requests-cache retry-requests numpy pandas
 
 import openmeteo_requests
-
+from pathlib import Path
 import requests_cache
 import pandas as pd
 from retry_requests import retry
-from pathlib import Path
 
 # Setup the Open-Meteo API client with cache and retry on error
 cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
@@ -19,7 +18,7 @@ url = "https://api.open-meteo.com/v1/forecast"
 params = {
 	"latitude": 52.3808,
 	"longitude": 4.6368,
-	"daily": ["temperature_2m_max", "temperature_2m_min", "sunrise", "sunset", "sunshine_duration", "precipitation_hours", "shortwave_radiation_sum"],
+	"daily": ["temperature_2m_max", "temperature_2m_min", "daylight_duration", "sunshine_duration", "uv_index_max", "uv_index_clear_sky_max", "precipitation_hours", "wind_speed_10m_max", "shortwave_radiation_sum"],
 	"past_days": 92,
 	"forecast_days": 1
 }
@@ -27,16 +26,22 @@ responses = openmeteo.weather_api(url, params=params)
 
 # Process first location. Add a for-loop for multiple locations or weather models
 response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
 # Process daily data. The order of variables needs to be the same as requested.
 daily = response.Daily()
 daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
 daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
-daily_sunrise = daily.Variables(2).ValuesAsNumpy()
-daily_sunset = daily.Variables(3).ValuesAsNumpy()
-daily_sunshine_duration = daily.Variables(4).ValuesAsNumpy()
-daily_precipitation_hours = daily.Variables(5).ValuesAsNumpy()
-daily_shortwave_radiation_sum = daily.Variables(6).ValuesAsNumpy()
+daily_daylight_duration = daily.Variables(2).ValuesAsNumpy()
+daily_sunshine_duration = daily.Variables(3).ValuesAsNumpy()
+daily_uv_index_max = daily.Variables(4).ValuesAsNumpy()
+daily_uv_index_clear_sky_max = daily.Variables(5).ValuesAsNumpy()
+daily_precipitation_hours = daily.Variables(6).ValuesAsNumpy()
+daily_wind_speed_10m_max = daily.Variables(7).ValuesAsNumpy()
+daily_shortwave_radiation_sum = daily.Variables(8).ValuesAsNumpy()
 
 daily_data = {"date": pd.date_range(
 	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
@@ -46,10 +51,12 @@ daily_data = {"date": pd.date_range(
 )}
 daily_data["temperature_2m_max"] = daily_temperature_2m_max
 daily_data["temperature_2m_min"] = daily_temperature_2m_min
-daily_data["sunrise"] = daily_sunrise
-daily_data["sunset"] = daily_sunset
+daily_data["daylight_duration"] = daily_daylight_duration
 daily_data["sunshine_duration"] = daily_sunshine_duration
+daily_data["uv_index_max"] = daily_uv_index_max
+daily_data["uv_index_clear_sky_max"] = daily_uv_index_clear_sky_max
 daily_data["precipitation_hours"] = daily_precipitation_hours
+daily_data["wind_speed_10m_max"] = daily_wind_speed_10m_max
 daily_data["shortwave_radiation_sum"] = daily_shortwave_radiation_sum
 
 daily_dataframe = pd.DataFrame(data = daily_data).assign(date=lambda d: d['date'].dt.strftime('%Y-%m-%d'))
--- scripts/merge.py
+++ scripts/merge.py
@@ -1,18 +1,16 @@
 import pandas as pd 
 
-df_hist = pd.read_csv("data/history.csv").assign(date=lambda d: pd.to_datetime(d['date']).dt.strftime("%Y-%m-%d"))
-df_annot = pd.read_csv("data/annots.tsv", delim_whitespace="\t")
+# Good grief this stuff is nasty. Should've moved to polars. 
+df_hist = pd.read_csv("data/history.csv").assign(date=lambda d: pd.to_datetime(pd.to_datetime(d['date']).dt.strftime("%Y-%m-%d")))
+df_annot = pd.read_csv("data/annots.tsv", sep="\t")
 
 df_annot_clean = (
     df_annot
       .assign(date=lambda d: pd.to_datetime("2024-04-01") + pd.to_timedelta(d['date'] - 45352, unit='D'))
-      .assign(date=lambda d: pd.to_datetime(d['date']).dt.strftime("%Y-%m-%d"))
+      .assign(date=lambda d: pd.to_datetime(pd.to_datetime(d['date']).dt.strftime("%Y-%m-%d")))
       .assign(taken=lambda d: d['levering_normal_tarif'] + d['levering_low_tarif'],
               supplied=lambda d: d['teruglevering_normal_tarif'] + d['teruglevering_low_tarif'])
       [['date', 'taken', 'supplied']]
 )
 
-print(df_hist.dtypes)
-print(df_annot_clean.dtypes)
-
-print(df_hist.join(df_annot_clean, on='date', how='left'))
+df_hist.merge(df_annot_clean, on='date', how='left').to_csv("data/merged.csv", index=False)
